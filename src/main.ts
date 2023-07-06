be0297931f43b26c7cc5d01e3ee8645cb570032b
Brayden <Braydenlwong@gmail.com>
2023-07-05 21:40:06 -0600

    removed some functions from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { ConfigService } from "@nestjs/config";
 import { Logger } from "@nestjs/common";
-import { createServer } from "https";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./http.exception.filter";
-import { WsAdapter } from "./ws.adapter";
 
 import cors from "cors";
 
@@ -12,27 +10,16 @@ async function bootstrap() {
   const AppLogger = new Logger("App");
   const WsLogger = new Logger("Websocket");
 
-  // const keyFile = fs.readFileSync(__dirname + "/../config/server.key");
-  // const certFile = fs.readFileSync(__dirname + "/../config/server.crt");
-
   const app = await NestFactory.create(AppModule);
-
-  const httpsServer = createServer();
 
   const config = app.get<ConfigService>(ConfigService);
   const PORT = config.get<number>("PORT");
-  const WS_PORT = config.get<number>("WS_PORT");
 
   app.use(cors());
   app.useGlobalFilters(new HttpExceptionFilter());
-  // app.useWebSocketAdapter(new WsAdapter(httpsServer));
   try {
     await app.listen(PORT, async () =>
       AppLogger.log(`Server is listening at ${await app.getUrl()}`),
-    );
-
-    httpsServer.listen(WS_PORT, () =>
-      WsLogger.log(`Websocket is listening at https://[::1]:${WS_PORT}`),
     );
   } catch (error) {
     AppLogger.error(`Error starting server: ${error}`);
