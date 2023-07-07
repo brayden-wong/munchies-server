a27ed7a1078329a8249baebde03b77f3cde1dda6
Brayden <Braydenlwong@gmail.com>
2023-07-07 15:49:33 -0600

    adding a bit more logs

--- src/modules/ws/ws.gateway.ts
+++ src/modules/ws/ws.gateway.ts
@@ -1,20 +1,37 @@
+import { UseGuards } from "@nestjs/common";
 import {
   WebSocketGateway,
   OnGatewayConnection,
   WebSocketServer,
   ConnectedSocket,
+  OnGatewayDisconnect,
 } from "@nestjs/websockets";
+import { C } from "drizzle-orm/db.d-cf0abe10";
 import type { Server, Socket } from "socket.io";
+import { WsGuard } from "./ws.guard";
+import { CurrentUserId } from "./decorators";
 
 @WebSocketGateway()
-export class WsGateway implements OnGatewayConnection {
+export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
+  @UseGuards(WsGuard)
   handleConnection(
     @ConnectedSocket()
     socket: Socket,
+    @CurrentUserId()
+    userId: string,
   ) {
     console.log(`socket ${socket.id} has connected`);
+    console.log(userId, "has connected");
+  }
+
+  handleDisconnect(
+    @ConnectedSocket()
+    socket: Socket,
+  ) {
+    console.log(`socket ${socket.id} has disconnected`);
+    socket.disconnect();
   }
 }
